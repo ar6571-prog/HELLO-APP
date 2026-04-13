6cee38d52dcabadebacd25ef1073434ebf750a07
ar6571-prog <ar6571@srmist.edu.in>
2026-04-13 08:27:13 +0530

    Create HelloApp that greets users

---
+++ HELLO APP.java
@@ -0,0 +1,24 @@
+public class HelloApp {
+    public static void main(String[] args) {
+
+        // Case 1: No arguments
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
+
+        // Case 2: Arguments present
+        StringBuilder nameBuilder = new StringBuilder();
+
+        // Enhanced for loop
+        for (String name : args) {
+            nameBuilder.append(name).append(", ");
+        }
+
+        // Remove trailing ", "
+        if (nameBuilder.length() > 0) {
+            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+            System.out.println("Hello, " + names + "!");
+        }
+    }
+}
